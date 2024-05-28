fa2ac31740ae840a624d7bde1924f42f1cb5518d
pipzgutz <pmgutierrez@pm.me>
2024-05-28 21:32:38 +0800

    init

---
+++ src/test/java/net/groundgurus/level2/spring_web_example/SpringWebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package net.groundgurus.level2.spring_web_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
